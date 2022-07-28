929ce73a05c3193e3d02b2cd4f0ac5a3b6d114e5
Jack <j.steventon@outlook.com>
2022-07-28 19:26:43 +0100

    Website framework complete

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,36 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from website import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_required, login_user, logout_user, current_user
 
 auth = Blueprint('auth', __name__,)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    return render_template("login.html")
+    if request.method=='POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user=user, remember=True)
+                flash('Logged in, successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password', category='error')
+        else:
+            flash('Account does not exist', category="error")
+
+
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return '<h1>Logout</h1>'
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def signUp():
@@ -21,7 +41,10 @@ def signUp():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
-        if len(email) < 1:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Account already exists', category='error')
+        elif len(email) < 1:
             flash('Please enter an email address', category='error')
         elif len(fName) < 1:
             flash('Please enter a First Name',category="error")
@@ -32,6 +55,11 @@ def signUp():
         elif password1 != password2:
             flash('Passwords do not match', category='error')
         else:
+            new_user = User(email=email, first_name=fName, last_name=lName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user=new_user, remember=True)
             flash('Account created!',category='success')
+            return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
--- website/models.py
+++ website/models.py
@@ -4,14 +4,15 @@ from flask_login import UserMixin
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
+    data = db.Column(db.String(10000), nullable=False)
     date = db.Column(db.DateTime(timezone=False), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
-    first_name = db.Column(db.String(50))
-    last_name = db.Column(db.String(50))
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
     notes = db.relationship('Note')
 
