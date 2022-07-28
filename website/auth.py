5794623694083bd76b27ae1663cfa92788def610
Jack <j.steventon@outlook.com>
2022-07-28 16:49:10 +0100

    Created Database Model

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__,)
 
-@auth.route('login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
     return render_template("login.html")
 
-@auth.route('logout')
+@auth.route('/logout')
 def logout():
     return '<h1>Logout</h1>'
 
-@auth.route('sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signUp():
+    if request.method=='POST':
+        email = request.form.get('email')
+        fName = request.form.get('fName')
+        lName = request.form.get('lName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 1:
+            flash('Please enter an email address', category='error')
+        elif len(fName) < 1:
+            flash('Please enter a First Name',category="error")
+        elif len(lName) < 1:
+            flash('Please enter a Last Name', category='error')
+        elif len(password1) < 7:
+            flash('Password must contain 7 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match', category='error')
+        else:
+            flash('Account created!',category='success')
+
     return render_template("sign_up.html")
---
+++ website/models.py
@@ -0,0 +1,17 @@
+from . import db
+from sqlalchemy.sql import func
+from flask_login import UserMixin
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=False), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    notes = db.relationship('Note')
+
