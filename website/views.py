a19f27aa306741c8f97e88b60a3115c9177b7d62
Jack <j.steventon@outlook.com>
2022-08-02 14:00:57 +0100

    Finished Note App

--- website/views.py
+++ website/views.py
@@ -1,9 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, flash, url_for
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
 
 views = Blueprint('views', __name__,)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method=='POST':
+        note = request.form.get('note')
+
+        if len(note)<1:
+            flash('Note is too short', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note Added', category='success')
+
     return render_template("home.html", user=current_user)
+
+
+@views.route('delete-note/<note_id>', methods=['POST'])
+@login_required
+def delete_note(note_id):
+    note = Note.query.get_or_404(note_id)
+    db.session.delete(note)
+    db.session.commit()
+    flash('Note Deleted', category="success")   
+    return redirect(url_for("views.home"))
+    
